03758bd79b983f6d40fdd7f89f37c41117a11902
Kevin Ji <kevin.ji@outlook.com>
2014-08-15 00:48:45 -0700

    Give BeepBop a proper name

--- src/helloworld/BeepBop.java
+++ src/helloworld/BeepBop.java
@@ -1,16 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package helloworld;
 
 /**
- *
+ * Beep bop!
  * @author Kevin Ji <kevin.ji@outlook.com>
  */
-public class ArrayTest {
+public class BeepBop {
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
             if (i % 15 == 0) {
