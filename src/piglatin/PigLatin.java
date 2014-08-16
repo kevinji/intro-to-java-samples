173fa1dd60c17c7bd298ece4cb58b09505fa5e6a
Kevin Ji <kevin.ji@outlook.com>
2014-08-16 14:54:47 -0700

    PigLatin: Add pigLatinPhrase

--- src/piglatin/PigLatin.java
+++ src/piglatin/PigLatin.java
@@ -7,6 +7,19 @@ package piglatin;
 public class PigLatin {
     private static final char[] VOWELS = {'a', 'e', 'i', 'o', 'u'};
     
+    public static String pigLatinPhrase(String phrase) {
+        String[] words = phrase.split(" ");
+        
+        for (int index = 0; index < words.length; index++) {
+            String word = words[index];
+            String newWord = pigLatinWord(word);
+            words[index] = newWord;
+        }
+        
+        String newPhrase = String.join(" ", words);
+        return newPhrase;
+    }
+    
     public static String pigLatinWord(String word) {
         char firstLetter = word.charAt(0);
         boolean startsWithVowel = isVowel(firstLetter);
@@ -32,7 +45,10 @@ public class PigLatin {
     }
     
     public static void main(String[] args) {
-        System.out.println(pigLatinWord("happy"));
-        System.out.println(pigLatinWord("egg"));
+        System.out.println(pigLatinWord("happy")); // appyhay
+        System.out.println(pigLatinWord("egg")); // eggway
+        
+        System.out.println(pigLatinPhrase("we are one big family"));
+        // eway areway oneway igbay amilyfay
     }
 }
