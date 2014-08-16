3e37f15e6ff70317078d4d7c446924c9958d443d
Kevin Ji <kevin.ji@outlook.com>
2014-08-16 14:33:56 -0700

    HighLowGame: Show lower/upper bounds with each guess

--- src/helloworld/HighLowGame.java
+++ src/helloworld/HighLowGame.java
@@ -14,15 +14,19 @@ public class HighLowGame {
         
         int guess = 0;
         int guessCount = 0;
+        int lowerBound = 1;
+        int upperBound = 100;
         
         while (guess != initialNum) {
-            guess = promptForGuess();
+            guess = promptForGuess(lowerBound, upperBound);
             guessCount++;
             
             if (guess < initialNum) {
                 System.out.println("Higher...");
+                lowerBound = guess + 1;
             } else if (guess > initialNum) {
                 System.out.println("Lower...");
+                upperBound = guess - 1;
             } else {
                 System.out.println("You got it!");
                 System.out.println("You took " + guessCount + " guesses.");
@@ -31,8 +35,9 @@ public class HighLowGame {
         }
     }
     
-    private static int promptForGuess() {
-        System.out.println("Guess a number:");
+    private static int promptForGuess(int lowerBound, int upperBound) {
+        System.out.println("Guess a number (" + lowerBound + "-" + upperBound +
+                "):");
         Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
     }
--- src/helloworld/HighLowGameStandalone.java
+++ src/helloworld/HighLowGameStandalone.java
@@ -13,15 +13,19 @@ public class HighLowGameStandalone {
         final int initialNum = (int)(Math.random() * 100) + 1;
         int guess = 0;
         int guessCount = 0;
+        int lowerBound = 1;
+        int upperBound = 100;
         
         while (guess != initialNum) {
-            guess = promptForGuess();
+            guess = promptForGuess(lowerBound, upperBound);
             guessCount++;
             
             if (guess < initialNum) {
                 System.out.println("Higher...");
+                lowerBound = guess + 1;
             } else if (guess > initialNum) {
                 System.out.println("Lower...");
+                upperBound = guess - 1;
             } else {
                 System.out.println("You got it!");
                 System.out.println("You took " + guessCount + " guesses.");
@@ -30,8 +34,9 @@ public class HighLowGameStandalone {
         }
     }
     
-    private static int promptForGuess() {
-        System.out.println("Guess a number:");
+    private static int promptForGuess(int lowerBound, int upperBound) {
+        System.out.println("Guess a number (" + lowerBound + "-" + upperBound +
+                "):");
         Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
     }
